feee6e751f66e861926aea21cb1bb6a6f21088b2
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2025-03-26 10:59:00 -0300

    Add button to manually start the guided tour in `GuidedTour.tsx`
    * Add `startTour` function to set `run` and `stepIndex`
    * Wrap `Joyride` component in a `div` with a button to start the tour
    * Style the button with Tailwind CSS classes

--- src/components/GuidedTour.tsx
+++ src/components/GuidedTour.tsx
@@ -51,20 +51,33 @@ const GuidedTour: React.FC = () => {
     }
   };
 
+  const startTour = () => {
+    setRun(true);
+    setStepIndex(0);
+  };
+
   return (
-    <Joyride
-      steps={steps}
-      run={run}
-      stepIndex={stepIndex}
-      callback={handleJoyrideCallback}
-      continuous
-      showSkipButton
-      styles={{
-        options: {
-          zIndex: 10000,
-        },
-      }}
-    />
+    <div>
+      <button
+        onClick={startTour}
+        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      >
+        Start Guided Tour
+      </button>
+      <Joyride
+        steps={steps}
+        run={run}
+        stepIndex={stepIndex}
+        callback={handleJoyrideCallback}
+        continuous
+        showSkipButton
+        styles={{
+          options: {
+            zIndex: 10000,
+          },
+        }}
+      />
+    </div>
   );
 };
 
